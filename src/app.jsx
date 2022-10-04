32692473ed5480a76f0185f6d0c1222c31849e0f
nnnnn <narimichi.matsumoto@gmail.com>
2022-10-04 13:42:36 +0000

    useState

--- src/app.jsx
+++ src/app.jsx
@@ -2,14 +2,27 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage.jsx";
 
 const App = () => {
-  const onClickBtn = () => {};
-  //const contentStyle = {color: "blue", fontSize:'18px'}
+  const [num, setNum] = useState(0);
+  const [showFlg, setShowFlg] = useState(true);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchFlg = () => {
+    setShowFlg(!showFlg);
+  };
+
   return (
     <>
       <h1 style={{ color: "red" }}>hello</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気？</ColorfulMessage>
-      <button onClick={onClickBtn}>BTN</button>
+      <button onClick={onClickCountUp}>Count up</button>
+      <br />
+      <button onClick={onClickSwitchFlg}>on/off</button>
+
+      <p>{num}</p>
+      {showFlg && <p>( ^^)</p>}
     </>
   );
 };
