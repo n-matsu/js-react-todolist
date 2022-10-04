7d7ec256a846a0e771e0d0427d7fb32b668cc7f9
nnnnn <narimichi.matsumoto@gmail.com>
2022-10-04 15:22:15 +0000

    Update app.jsx and index.js

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,6 @@
-import React, { useState } from "react";
-import ColorfulMessage from "./components/ColorfulMessage.jsx";
+import React from "react";
+import ReactDom from "react-dom";
 
-const App = () => {
-  const [num, setNum] = useState(0);
-  const [showFlg, setShowFlg] = useState(true);
-
-  const onClickCountUp = () => {
-    setNum(num + 1);
-  };
-  const onClickSwitchFlg = () => {
-    setShowFlg(!showFlg);
-  };
-
-  return (
-    <>
-      <h1 style={{ color: "red" }}>hello</h1>
-      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
-      <ColorfulMessage color="pink">元気？</ColorfulMessage>
-      <button onClick={onClickCountUp}>Count up</button>
-      <br />
-      <button onClick={onClickSwitchFlg}>on/off</button>
-
-      <p>{num}</p>
-      {showFlg && <p>( ^^)</p>}
-    </>
-  );
+export const App = () => {
+  return <></>;
 };
-
-export default App;
